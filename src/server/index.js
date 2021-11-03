87c4cd9010dced3201d00d96aff9808b2221df6e
Isaac Blanco <nuclearhands2001@gmail.com>
2021-11-03 15:12:20 -0500

    basic routes

--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,77 @@
-const app = require('express')();
+const qr = require('qrcode');
+const express = require('express')
+const app = express();
+const session = require('express-session');
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 80; 
 
-app.post('',(req,res) =>{
+// Sockets
 
-});
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 app.get('/',(req,res) =>{
-    res.send('Hello World!');
+    if(req.session.loggedin){
+        response.send('Welcome back, ' + req.session.username + '!');
+    }else{
+        res.send('Server is working!');
+    }
+    res.end();
+});
+
+app.get('/login',(req,res) =>{
+    // Login res.send()
+});
+
+// Schedule list
+app.post('/schedule',(req,res) =>{
+
+})
+
+// Login
+app.post('/auth',(req,res) =>{
+    let username = 'isaac';
+    let password = '123';
+    if(username && password){
+        // query para verificar existencia
+        req.session.loggedin = true;
+        req.session.username = username;
+    }else{
+        res.send('Please enter Username and password!');
+        res.end();
+    }
+})
+
+// attendace
+app.post('/checkList',(req,res) =>{
+
+})
+
+// Classroom
+app.get('/join_class/:id',(req,res) => {
+    let class_id = req.params.id;
+    // Query para verificar existencia de la clase
+    // QR profesor
+    // QR clase
+    // QR profesor + Qr clase = Asistencia
+    /*
+    qr.toFile('./class.png',class_id,{
+        color: {
+            dark: '#FFF',
+            light: '#0000'
+        }
+    }
+    ,function(err){
+        if(err) throw err
+        console.log('Code generated')
+    })
+    */
 });
 
 app.listen(port, () =>{
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     module:{
         rules:[
             {
-                test: /\.js$/,
+                test: /\.js$|\.jsx$/,
                 include: [path.resolve(__dirname,'src')]
             }
         ]
