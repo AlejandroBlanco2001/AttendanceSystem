970c3a05239d726f95800f0a01e2b3e5ed02b0c0
Isaac Blanco <nuclearhands2001@gmail.com>
2021-11-23 16:58:16 -0500

    fix routes

--- src/server/routes/class.js
+++ src/server/routes/class.js
@@ -16,47 +16,55 @@ router.get('/subject/:id', async (req,res) => {
 });
 
 router.post('/getAttendance', async (req,res) => {
-    let {code} = req.body;
-    let conn = await db.pool.getConnection();
-    let result = await util.getAttendanceClass(conn,code);
-    conn.end();
-    if(result == 0) res.sendStatus(500);
-    else{
-        result[0].logAttendance = DateTime.fromJSDate(result[0].logAttendance).toLocaleString(DateTime.DATETIME_SHORT);
-        res.json(result);
+    if(req.user){
+        if(req.user.type == '1'){
+            let {code} = req.body;
+            let conn = await db.pool.getConnection();
+            let result = await util.getAttendanceClass(conn,code);
+            conn.end();
+            if(result == 0) res.sendStatus(500);
+            else{
+                result[0].logAttendance = DateTime.fromJSDate(result[0].logAttendance).toLocaleString(DateTime.DATETIME_SHORT);
+                res.json(result);
+            }
+        }
     }
 })
 
 router.post('/takeAttendance', async (req,res) => {
-    let {id_class, id_teacher} = req.body;
-    id_class = id_class.split('\/')[1];
-    let conn = await db.pool.getConnection();
-    let result = await util.getHourClass(conn,{code: id_class, teach: id_teacher});
-    let resClass = await util.checkCodesClass(conn,id_class);
-    let resTeach = await util.checkCodesTeacher(conn,id_teacher);
-    conn.end();
-    let hour = DateTime.fromJSDate(result);
-    let curr = DateTime.now();
-    let limit_assist = hour.plus({minutes: 10});
-    let limit_late = hour.plus({minutes: 20});
-    if(resClass != -1 && resTeach != -1){
-        if(curr > hour){
+    if(req.user){
+        if(req.user.type == '2'){
+            let {id_class, id_teacher} = req.body;
+            id_class = id_class.split('\/')[1];
             let conn = await db.pool.getConnection();
-            let clas = await util.getClassSession(conn,{code: id_class, teach: id_teacher});
-            if(curr < limit_assist){
-                await conn.query('INSERT INTO clas_stud VALUES(?,?,?,?)',[clas,req.user.id_pers," ",curr.toLocaleString(DateTime.DATETIME_SHORT_WITH_SECONDS)]);
-                asiss = ' ';
-            }else if(curr < limit_late){
-                await conn.query('INSERT INTO clas_stud VALUES(?,?,?,?)',[clas,req.user.id_pers,"-",curr.toLocaleString(DateTime.DATETIME_SHORT_WITH_SECONDS)])
-                asiss = '-';
-            }else{
-                await conn.query('INSERT INTO clas_stud VALUES(?,?,?,?)',[clas,req.user.id_pers,"+",curr.toLocaleString(DateTime.DATETIME_SHORT_WITH_SECONDS)])
-                asiss = '+';
-            }
+            let result = await util.getHourClass(conn,{code: id_class, teach: id_teacher});
+            let resClass = await util.checkCodesClass(conn,id_class);
+            let resTeach = await util.checkCodesTeacher(conn,id_teacher);
             conn.end();
-            res.sendStatus(200);
-        }else{
-            res.send('To early');
+            let hour = DateTime.fromJSDate(result);
+            let curr = DateTime.now();
+            let limit_assist = hour.plus({minutes: 10});
+            let limit_late = hour.plus({minutes: 20});
+            if(resClass != -1 && resTeach != -1){
+                if(curr > hour){
+                    let conn = await db.pool.getConnection();
+                    let clas = await util.getClassSession(conn,{code: id_class, teach: id_teacher});
+                    if(curr < limit_assist){
+                        await conn.query('INSERT INTO clas_stud VALUES(?,?,?,?)',[clas,req.user.id_pers," ",curr.toLocaleString(DateTime.DATETIME_SHORT_WITH_SECONDS)]);
+                        asiss = ' ';
+                    }else if(curr < limit_late){
+                        await conn.query('INSERT INTO clas_stud VALUES(?,?,?,?)',[clas,req.user.id_pers,"-",curr.toLocaleString(DateTime.DATETIME_SHORT_WITH_SECONDS)])
+                        asiss = '-';
+                    }else{
+                        await conn.query('INSERT INTO clas_stud VALUES(?,?,?,?)',[clas,req.user.id_pers,"+",curr.toLocaleString(DateTime.DATETIME_SHORT_WITH_SECONDS)])
+                        asiss = '+';
+                    }
+                    conn.end();
+                    res.sendStatus(200);
+                }else{
+                    res.send('To early');
+                }
+            }
         }
     }
 })
