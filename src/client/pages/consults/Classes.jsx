a0fc85ea405d866a654a0fe76887a78e5a52c244
Alvaro <calvaroj@uninorte.edu.co>
2021-11-23 03:28:44 -0500

    Added the functions to update and erase

--- src/client/pages/consults/Classes.jsx
+++ src/client/pages/consults/Classes.jsx
@@ -6,12 +6,26 @@ import "../../styles/table.css";
 const Class= () => {
   const [isData, setIsData] = useState(false);
   const [classes, setClasses] = useState([]);
-  const [updatedUser, setUpdatedUser] = useState({})
+  const [updatedUser, setUpdatedClasses] = useState({})
   const [listaClasses, setListaClasses] = useState([]);
   const [needUpdate, setNeedUpdate] = useState(false);
   const [code, setCodeCl]= useState(0);
   const [start_time, setStartTimeCl]= useState();
   const [code_spac, setCodeSpac]= useState("");
+
+  const handleDeleteClass = (clas) => {
+    deleteClass(clas.code);
+  }
+  
+  const handleUpdateClass = (clas) => {
+    setUpdatedClass({});
+    let inputs = document.getElementsByTagName("input");
+    for (let index = 0; index < inputs.length; ++index) {
+      inputs[index].value = ""
+    }
+    console.log("USER TO UPDATE: ", clas);
+    setUpdatedClass(clas);
+  }
 
   const addClass = () => {
     axios.post("http://localhost:80/admin/create/classes", {
@@ -37,31 +51,30 @@ const Class= () => {
       });
   };
 
+  const deleteClass = (id) => {
+    axios.post(`http://localhost:80/admin/delete/classes/${id}`, { 1: true }, { withCredentials: true })
+      .then((response) => {
+        console.log("Eliminado correctamente");
+        console.log("RESPONSE: ", response);
+        window.location.reload(true);
+        reload();
+      })
+      .catch((err) => {
+        console.log("ERROR ELIMINANDO");
+        console.log(err);
+      });
+  };
 
-  // const deletePadre = (cedula) => {
-  //   Axios.delete(`http://localhost:3004/deletepadre/${cedula}`)
-  //     .then((response) => {
-  //       console.log("Eliminado correctamente");
-  //       window.location.reload(false);
-  //     })
-  //     .catch((err) => {
-  //       console.log("ERROR ELIMINANDO");
-  //       console.log(err);
-  //     });
-  // };
-
-  // const updateUser = () => {
-  //   Axios.put("http://localhost:3004/update", {
-  //     username: username|| updatedUser.username,
-  //     passcode: passcode || updatedUser.passcode,
-  //     urlimage: urlimage || updatedUser.urlimage,
-  //     id_person: id_person || updatedUser.id_person,
-  //     // Se necesita en caso de que el usuario cambie la cedula en el input o para comparar la informacion actual con la anterior
-  //   }).then((response) => {
-  //     window.location.reload(false);
-  //     reload();
-  //   });
-  // };
+  const updateClass = () => {
+    axios.post(`http://localhost:80/admin/update/subjects/${updatedUser.code}`, {
+      code: code|| updatedUser.code,
+      start_time: start_time || updatedUser.start_time,
+      code_spac: code_spac || updatedUser.code,
+    }, { withCredentials: true }).then((response) => {
+      window.location.reload(true);
+      reload();
+    });
+  };
 
 useEffect(() => {
   axios
@@ -82,13 +95,13 @@ const sendInfo = (e) => {
 
 
  return (
- <main>
- <form action="" className="login-form" onSubmit={sendInfo}>
+ <main className="main-users">
+ <form onSubmit={sendInfo}>
           <img  alt="" />
           <div className="form-block">
             <label htmlFor="code">Class' Code</label>
-            <input
-              type="texte"
+            <input className="row-form"
+              type="text"
               id="code"
               value={code || "" || updatedUser.code}
               placeholder="type the code of the class"
@@ -98,8 +111,8 @@ const sendInfo = (e) => {
           </div>
           <div className="form-block">
             <label htmlFor="start_time">Start Time</label>
-            <input
-              type="texte"
+            <input className="row-form"
+              type="text"
               placeholder="type the start time of the class"
               id="start_time"
               value={start_time || "" || updatedUser.start_time}
@@ -109,8 +122,8 @@ const sendInfo = (e) => {
           </div>
           <div className="form-block">
             <label htmlFor="code_spac">Space's Code</label>
-            <input
-              type="texte"
+            <input className="row-form"
+              type="text"
               placeholder="type the code of the Space"
               id="code_spac"
               value={code_spac || "" || updatedUser.code_spac}
@@ -125,8 +138,7 @@ const sendInfo = (e) => {
           />
           <button
            className="button primary-button button-row"
-          // onClick={updateUser}
-          disabled={!needUpdate}
+          onClick={updateClass}
           type="button"
         >
         ACTUALIZAR
@@ -143,8 +155,8 @@ const sendInfo = (e) => {
           ]}
           data={isData?classes:null}
           setNeedUpdate={setNeedUpdate}
-          handleDeleteUser={null}
-          handleUpdateUser={null}
+          handleDeleteElement={handleDeleteClass}
+          handleUpdateElement={handleUpdateClass}
         />
     </div>
     </main>
--- src/client/pages/consults/Enrollments.jsx
+++ src/client/pages/consults/Enrollments.jsx
@@ -6,13 +6,27 @@ import "../../styles/table.css";
 const Enrollment= () => {
   const [isData, setIsData] = useState(false);
   const [enrollments, setEnrollments] = useState([]);
-  const [updatedUser, setUpdatedUser] = useState({})
+  const [updatedUser, setUpdatedEnrollment] = useState({})
   const [listaEnrollments, setListaEnrollments] = useState([]);
   const [needUpdate, setNeedUpdate] = useState(false);
   const [id_stud, setIdStudEr]= useState("");
   const [year_peri, setYearPeriEr]= useState("");
   const [term_peri, setTermPeriEr]= useState("");
-  
+
+  const handleDeleteEnrollment = (enroll) => {
+    deleteEnrollment([enroll.code,enroll.id_stud]);
+  }
+
+  const handleUpdateEnrollment = (enroll) => {
+    setUpdatedEnrollment({});
+    let inputs = document.getElementsByTagName("input");
+    for (let index = 0; index < inputs.length; ++index) {
+      inputs[index].value = ""
+    }
+    console.log("USER TO UPDATE: ", enroll);
+    setUpdatedEnrollment(enroll);
+  }
+
 
   const addEnrollment = () => {
     axios.post("http://localhost:80/admin/create/enrollments", {
@@ -38,32 +52,30 @@ const Enrollment= () => {
       });
   };
 
+const deleteEnrollment = (id) => {
+    axios.post(`http://localhost:80/admin/delete/enrollments/${id[0]}:${id[1]}`, { 1: true }, { withCredentials: true })
+      .then((response) => {
+        console.log("Eliminado correctamente");
+        console.log("RESPONSE: ", response);
+        window.location.reload(true);
+        reload();
+      })
+      .catch((err) => {
+        console.log("ERROR ELIMINANDO");
+        console.log(err);
+      });
+  };
 
-  // const deletePadre = (cedula) => {
-  //   Axios.delete(`http://localhost:3004/deletepadre/${cedula}`)
-  //     .then((response) => {
-  //       console.log("Eliminado correctamente");
-  //       window.location.reload(false);
-  //     })
-  //     .catch((err) => {
-  //       console.log("ERROR ELIMINANDO");
-  //       console.log(err);
-  //     });
-  // };
-
-  // const updateUser = () => {
-  //   Axios.put("http://localhost:3004/update", {
-  //     username: username|| updatedUser.username,
-  //     passcode: passcode || updatedUser.passcode,
-  //     urlimage: urlimage || updatedUser.urlimage,
-  //     id_person: id_person || updatedUser.id_person,
-  //     // Se necesita en caso de que el usuario cambie la cedula en el input o para comparar la informacion actual con la anterior
-  //   }).then((response) => {
-  //     window.location.reload(false);
-  //     reload();
-  //   });
-  // };
-
+  const updateEnrollment = () => {
+    axios.post(`http://localhost:80/admin/update/enrollments/${updatedUser.id}:${updatedUser.id_stud}`, {
+     id_stud: id_stud|| updatedUser.id_stud,
+      year_peri: year_peri || updatedUser.year_peri,
+     term_peri: term_peri || updatedUser.term_peri
+    }, { withCredentials: true }).then((response) => {
+      window.location.reload(true);
+      reload();
+    });
+  };
 useEffect(() => {
   axios
   .get("http://localhost:80/admin/enrollments", { withCredentials: true })
@@ -83,13 +95,13 @@ const sendInfo = (e) => {
 
 
  return (
- <main>
- <form action="" className="login-form" onSubmit={sendInfo}>
+ <main className="main-users">
+ <form onSubmit={sendInfo}>
           <img  alt="" />
           <div className="form-block">
             <label htmlFor="id_stud">Student's ID</label>
-            <input
-              type="texte"
+            <input className="row-form"
+              type="text"
               id="id_stud"
               value={id_stud || "" || updatedUser.id_stud}
               placeholder="type the ID of the Student"
@@ -99,8 +111,8 @@ const sendInfo = (e) => {
           </div>
           <div className="form-block">
             <label htmlFor="year_peri">Period's Year</label>
-            <input
-              type="texte"
+            <input className="row-form"
+              type="text"
               placeholder="type the year of the Period"
               id="year_peri"
               value={year_peri || "" || updatedUser.year_peri}
@@ -110,8 +122,8 @@ const sendInfo = (e) => {
           </div>
           <div className="form-block">
             <label htmlFor="year_termEr">Period's Term</label>
-            <input
-              type="texte"
+            <input className="row-form"
+              type="text"
               placeholder="type the term of the Period"
               id="year_termEr"
               value={term_peri || "" || updatedUser.term_peri}
@@ -126,8 +138,8 @@ const sendInfo = (e) => {
           />
           <button
            className="button primary-button button-row"
-          // onClick={updateUser}
-          disabled={!needUpdate}
+          onClick={updateEnrollment}
+         
           type="button"
         >
         ACTUALIZAR
@@ -145,8 +157,8 @@ const sendInfo = (e) => {
           ]}
           data={isData?enrollments:null}
           setNeedUpdate={setNeedUpdate}
-          handleDeleteUser={null}
-          handleUpdateUser={null}
+          handleDeleteElement={handleDeleteEnrollment}
+          handleUpdateElement={handleUpdateEnrollment}
         />
     </div>
     </main>
